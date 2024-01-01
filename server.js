c8dbb2ea8e5286aa55cf2a1d4f133321b2ba2736
DimRev <dimarev444@gmail.com>
2024-01-01 15:48:38 +0200

    Edit changed environment switching logic

--- server.js
+++ server.js
@@ -1,15 +1,8 @@
 import express from 'express'
 import cookieParser from 'cookie-parser'
 import cors from 'cors'
-import path,{dirname} from 'path'
-import { fileURLToPath } from 'url'
+import path from 'path'
 
-import { toyService } from './service/toy.service.js'
-import { userService } from './service/user.service.js'
-import { dashboardService } from './service/dashboard.service.js'
-
-const __filename = fileURLToPath(import.meta.url)
-const __dirname = dirname(__filename)
 
 import { loggerService } from './service/logger.service.js'
 loggerService.info('server.js loaded...')
@@ -21,27 +14,21 @@ app.use(cookieParser())
 app.use(express.json())
 app.use(express.static('public'))
 
-if(process.env.NODE_ENV === 'production'){
-  //Express serve static files on production env
-  app.use(express.static(path.resolve(__dirname, 'public')))
-  console.log('__dirname: ', __dirname)
+console.log(process.env.NODE_ENV)
+
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static(path.resolve('public')))
 } else {
-  //Configure CORS
   const corsOptions = {
-    //Make sure origin contains the url of your frontend domain
-    origin: [
-      'http://127.0.0.1:8080',
-      'http://localhost:8080',
-      'http://127.0.0.1:5173',
-      'http://localhost:5173',
-    ],
-    credentials: true,
+      origin: [   'http://127.0.0.1:3000',
+                  'http://localhost:3000',
+                  'http://127.0.0.1:5173',
+                  'http://localhost:5173'
+              ],
+      credentials: true
   }
   app.use(cors(corsOptions))
 }
-
-
-
 
 import { toyRoutes } from './api/toy/toy.routes.js'
 app.use('/api/toy', toyRoutes)
@@ -54,122 +41,6 @@ app.use('/api/auth', authRoutes)
 
 import { dashboardRoutes } from './api/dashboard/dashboard.routes.js'
 app.use('/api/dashboard', dashboardRoutes)
-
-// //Get Toys
-// app.get('/api/toy/', (req, res) => {
-//   //get query params to filterBy
-//   const filterBy = req.query.filterBy
-//   const sortBy = req.query.sortBy
-
-//   toyService
-//     .query(filterBy, sortBy)
-//     .then((toys) => {
-//       res.send(toys)
-//     })
-//     .catch((err) => {
-//       loggerService.error('Cannot get toys', err)
-//       res.status(404).send('cannot get toys')
-//     })
-// })
-
-// //Add Toy
-// app.post('/api/toy/', (req, res) => {
-//   const { name, price, labels, createdAt, inStock, img, stock, inventory } =
-//     req.body
-//   const toyToSave = {
-//     name,
-//     price,
-//     labels,
-//     createdAt,
-//     inStock,
-//     img,
-//     stock,
-//     inventory,
-//   }
-//   toyService
-//     .save(toyToSave)
-//     .then((toy) => res.send(toy))
-//     .catch((err) => {
-//       loggerService.error('Cannot save toy', err)
-//       res.status(404).send('cannot save toy')
-//     })
-// })
-
-// //Edit Toy
-// app.put('/api/toy/', (req, res) => {
-//   const {
-//     _id,
-//     name,
-//     price,
-//     labels,
-//     createdAt,
-//     inStock,
-//     img,
-//     stock,
-//     inventory,
-//   } = req.body
-//   const toyToSave = {
-//     _id,
-//     name,
-//     price,
-//     labels,
-//     createdAt,
-//     inStock,
-//     img,
-//     stock,
-//     inventory,
-//   }
-//   toyService
-//     .save(toyToSave)
-//     .then((toy) => res.send(toy))
-//     .catch((err) => {
-//       loggerService.error('Cannot save toy', err)
-//       res.status(404).send('cannot save toy')
-//     })
-// })
-
-// //Get Toy
-// app.get('/api/toy/:toyId', (req, res) => {
-//   const toyId = req.params.toyId
-//   toyService
-//     .getById(toyId)
-//     .then((toy) => {
-//       res.send(toy)
-//     })
-//     .catch((err) => {
-//       loggerService.error('Cannot get toy', err)
-//       res.status(404).send('Cannot get toy')
-//     })
-// })
-
-// //Remove Toy
-// app.delete('/api/toy/:toyId', (req, res) => {
-//   const toyId = req.params.toyId
-//   toyService
-//     .remove(toyId)
-//     .then((toy) => res.send(toy))
-//     .catch((err) => {
-//       loggerService.error('cannot remove toy', err)
-//       res.status(404).send('Cannot remove toy')
-//     })
-// })
-
-//Get Dashboard Data
-
-// app.get('/api/dashboard/', (req, res) => {
-//   //get query params to filterBy
-//   const sortBy = req.query.sortBy
-
-//   dashboardService
-//     .query()
-//     .then((data) => {
-//       res.send(data)
-//     })
-//     .catch((err) => {
-//       loggerService.error('Cannot get dashboard data', err)
-//       res.status(404).send('cannot get dashboard data')
-//     })
-// })
 
 app.get('/**', (req, res) => {
   res.sendFile(path.resolve('public/index.html'))
