8362ae6773584b181b4a9b76f2b7c11079f8b8c3
DimRev <dimarev444@gmail.com>
2024-01-01 15:53:44 +0200

    Fix trying to fix production bug of page not rendering

--- config/dev.js
+++ config/dev.js
@@ -1,7 +1,5 @@
-import { ADMIN_PASSWORD } from "../globalVars.js";
-
 export default {
-    dbURL: `mongodb+srv://Admin:${ADMIN_PASSWORD}@mistertoy.ku3ejxj.mongodb.net/`,
+    dbURL: `mongodb+srv://Admin:${process.env.ADMIN_PASSWORD}@mistertoy.ku3ejxj.mongodb.net/`,
     // dbURL: 'mongodb://localhost:27017',
     dbName: 'toys_db',
 }
